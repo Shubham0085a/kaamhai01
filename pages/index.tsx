d0dd237f37205d27cf878768efbbf732bb4a491f
Shubham0085a <aasy0085@gmail.com>
2024-05-21 19:10:50 +0530

    responsiv changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import MonthlyPayslip from "@/public/Monthly Payslip.png";
 import Kochi from "@/public/Kochi.png";
 import cuate from "@/public/cuate.png";
 import Logo from "@/public/logo.png";
+import Employee from "@/public/employee.png";
 import PlayStore from "@/public/Google Play Badge.png";
 import AppStore from "@/public/App Store Badge.png";
 import EmployeeCard from "@/public/Employee card.png";
@@ -22,6 +23,7 @@ import { FaSearch } from "react-icons/fa";
 import { IoIosStar } from "react-icons/io";
 import Marquee from "react-fast-marquee";
 import Ripple from "@/components/ripple";
+import { FaWhatsapp } from "react-icons/fa6";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,9 +40,17 @@ export default function Home() {
               Let&#39;s Talk Business!
             </a>
           </nav>
-          <button className="inline-flex items-center bg-[#FFD704] text-black py-1 px-3 border-2 font-bold border-black focus:outline-none hover:bg-gray-200 text-base mt-4 md:mt-0">
+          {/* <button className="inline-flex items-center bg-[#FFD704] text-black py-1 px-3 border-2 font-bold border-black focus:outline-none hover:bg-gray-200 text-base mt-4 md:mt-0">
             Download the App
-          </button>
+          </button> */}
+          <div className=" flex gap-6">
+            <button>
+              <Image src={AppStore} alt=""></Image>
+            </button>
+            <button>
+              <Image src={PlayStore} alt=""></Image>
+            </button>
+          </div>
         </div>
       </header>
       {/* Hero Section */}
@@ -48,7 +58,10 @@ export default function Home() {
         {/* Hero Heading */}
         <div className="flex flex-col gap-10">
           <h2 className=" font-extrabold sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl leading-snug sm:px-40 md:px-52 lg:px-60 xl:px-72 2xl:px-80 items-center justify-center text-center">
-            Bring your hiring journey with <span className="bg-vector1 bg-no-repeat bg-contain bg-center">Kaamhai</span>
+            Bring your hiring journey with{" "}
+            <span className="bg-vector1 bg-no-repeat bg-contain bg-center">
+              Kaamhai
+            </span>
           </h2>
           <p className=" text-3xl sm:px-[5rem] md:px-[12rem] lg:px-[20rem] xl:px-[28rem] 2xl:px-[38rem] leading-snug items-center justify-center text-center">
             Upgrade your hiring process with Kaamhai Designed with verified
@@ -80,7 +93,7 @@ export default function Home() {
           </div>
         </div>
         {/* Scroll Section */}
-        <div className=" bg-[#FFD704] border flex border-black border-t-4 border-b-4 border-l-0 border-r-0 w-full h-20">
+        {/* <div className=" bg-[#FFD704] border flex border-black border-t-4 border-b-4 border-l-0 border-r-0 w-full h-20">
           <Marquee>
             <div className=" mr-10 font-bold text-5xl uppercase">
               <h1>Empowerment</h1>
@@ -119,13 +132,16 @@ export default function Home() {
               <IoIosStar />
             </div>
           </Marquee>
-        </div>
+        </div> */}
         {/* Hero Description */}
         <div className="flex flex-row items-center px-10 justify-center">
           <div className="flex flex-col w-[80%] md:w-[70%] lg:w-[60%] xl:w-[47%] 2xl:w-[47%] xl:mt-10">
             <h1 className="font-extrabold text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-8xl leading-snug capitalize text-[#9E9E9E] text-wrap">
               We&#39;ll help you make{" "}
-              <span className=" text-black bg-vector1 bg-no-repeat bg-contain bg-center">smarter</span> hiring decisions
+              <span className=" text-black bg-vector1 bg-no-repeat bg-contain bg-center">
+                smarter
+              </span>{" "}
+              hiring decisions
             </h1>
             <p className="text-3xl leading-snug items-center justify-center">
               To access the app, just open the link we&#39;ll send you{" "}
@@ -186,30 +202,91 @@ export default function Home() {
       </div>
       {/* Business Operations */}
       <div className="bg-[linear-gradient(180deg,_#F4F4F4_0%,_#FFFFFF_100%)]">
-        <div className="flex flex-col flex-wrap items-center justify-center">
-          <h1 className=" text-5xl font-bold text-center py-16 leading-normal">
-            Simplify Your Business Operations <br /> with Ease!
-          </h1>
-          <div className="flex lg:flex-row flex-col  text-3xl font-bold px-10 pb-16 w-[75%] gap-10 items-center justify-center">
-            <div className="flex flex-row items-center lg:border-none border-b-2 lg:pb-0 pb-20 border-gray-600">
-            <div className=" flex flex-col items-center gap-14">
-              <Image src={LeaveManagementSystem} alt=""></Image>
-              <h1 className=" text-center">Leave Management<br/> System</h1>
+        <div className="flex flex-row-reverse items-center justify-center">
+          <div className=" ">
+            <Image src={Employee} alt=""></Image>
+          </div>
+          <div className="flex flex-col flex-wrap items-center justify-center">
+            <h1 className=" text-5xl font-bold text-start py-16 leading-normal">
+              Simplify Your Business Operations <br /> with Ease!
+            </h1>
+            <div className="flex lg:flex-row flex-col  text-3xl font-bold px-10 pb-16  gap-10 items-center justify-center">
+              <div className="flex flex-row items-center lg:border-none border-b-2 lg:pb-0 pb-20 border-gray-600">
+                <div className=" flex flex-col items-center gap-14">
+                  <Image src={LeaveManagementSystem} alt=""></Image>
+                  <h1 className=" text-center">
+                    Leave Management
+                    <br /> System
+                  </h1>
+                </div>
+                <div className=" flex flex-col-reverse -mb-32 items-center gap-14">
+                  <Image src={AdvancePayTracker} alt=""></Image>
+                  <h1 className=" text-center">
+                    Advance Pay
+                    <br /> Tracker
+                  </h1>
+                </div>
+              </div>
+              <div className="flex flex-row items-center">
+                <div className=" flex flex-col items-center gap-14">
+                  <Image src={PaymentTrackers} alt=""></Image>
+                  <h1 className=" text-center">
+                    Payment
+                    <br /> Trackers
+                  </h1>
+                </div>
+                <div className=" flex flex-col-reverse -mb-32 items-center gap-14">
+                  <Image src={MonthlyPayslip} alt=""></Image>
+                  <h1 className=" text-center">
+                    Monthly
+                    <br /> Payslip
+                  </h1>
+                </div>
+              </div>
             </div>
-            <div className=" flex flex-col-reverse -mb-32 items-center gap-14">
-              <Image src={AdvancePayTracker} alt=""></Image>
-              <h1 className=" text-center">Advance Pay<br/> Tracker</h1>
-            </div>
-            </div>
-            <div className="flex flex-row items-center">
-            <div className=" flex flex-col items-center gap-14">
-              <Image src={PaymentTrackers} alt=""></Image>
-              <h1 className=" text-center">Payment<br/> Trackers</h1>
-            </div>
-            <div className=" flex flex-col-reverse -mb-32 items-center gap-14">
-              <Image src={MonthlyPayslip} alt=""></Image>
-              <h1 className=" text-center">Monthly<br/> Payslip</h1>
-            </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center">
+          <div className=" ">
+            <Image src={Employee} alt=""></Image>
+          </div>
+          <div className="flex flex-col flex-wrap items-center justify-center">
+            <h1 className=" text-5xl font-bold text-end py-16 leading-normal">
+            Caption for Employees
+            </h1>
+            <div className="flex lg:flex-row flex-col  text-3xl font-bold px-10 pb-16  gap-10 items-center justify-center">
+              <div className="flex flex-row items-center lg:border-none border-b-2 lg:pb-0 pb-20 border-gray-600">
+                <div className=" flex flex-col items-center gap-14">
+                  <Image src={LeaveManagementSystem} alt=""></Image>
+                  <h1 className=" text-center">
+                    Leave Management
+                    <br /> System
+                  </h1>
+                </div>
+                <div className=" flex flex-col-reverse -mb-32 items-center gap-14">
+                  <Image src={AdvancePayTracker} alt=""></Image>
+                  <h1 className=" text-center">
+                    Advance Pay
+                    <br /> Tracker
+                  </h1>
+                </div>
+              </div>
+              <div className="flex flex-row items-center">
+                <div className=" flex flex-col items-center gap-14">
+                  <Image src={PaymentTrackers} alt=""></Image>
+                  <h1 className=" text-center">
+                    Payment
+                    <br /> Trackers
+                  </h1>
+                </div>
+                <div className=" flex flex-col-reverse -mb-32 items-center gap-14">
+                  <Image src={MonthlyPayslip} alt=""></Image>
+                  <h1 className=" text-center">
+                    Monthly
+                    <br /> Payslip
+                  </h1>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -272,6 +349,37 @@ export default function Home() {
             Feel free to chat with our support team whenever <br /> you need
             more clarity
           </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+          >
+            <path
+              fill="#fff"
+              d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z"
+            ></path>
+            <path
+              fill="#fff"
+              d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z"
+            ></path>
+            <path
+              fill="#cfd8dc"
+              d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z"
+            ></path>
+            <path
+              fill="#40c351"
+              d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z"
+            ></path>
+            <path
+              fill="#fff"
+              fill-rule="evenodd"
+              d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </div>
       </div>
       {/* Footer */}
@@ -336,12 +444,15 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-10">
             <h1 className="text-xl font-extrabold">Get the App</h1>
-            <Image  src={AppStore} alt=""></Image>
+            <Image src={AppStore} alt=""></Image>
             <Image src={PlayStore} alt=""></Image>
           </div>
         </div>
         <div>
-          <p>© 2024 <span className=" font-bold">Kaamhai</span>. All rights reserved I Made with 💙</p>
+          <p>
+            © 2024 <span className=" font-bold">Kaamhai</span>. All rights
+            reserved I Made with 💙
+          </p>
         </div>
       </footer>
     </main>
